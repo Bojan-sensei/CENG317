984730bd7e212538314ee270af34f8d04c4f12db
Bojan-sensei <31077566+Bojan-sensei@users.noreply.github.com>
2018-03-11 23:52:24 -0400

    Added Stepper Timer

---
+++ firmware/stepperTimer.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+enable_pin = 25
+coil_A_1_pin = 20
+coil_A_2_pin = 21
+coil_B_1_pin = 19
+coil_B_2_pin = 26
+
+GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(coil_A_1_pin, GPIO.OUT)
+GPIO.setup(coil_A_2_pin, GPIO.OUT)
+GPIO.setup(coil_B_1_pin, GPIO.OUT)
+GPIO.setup(coil_B_2_pin, GPIO.OUT)
+
+GPIO.output(enable_pin, 1)
+
+starttime=time.time()
+
+def forward(delay, steps):
+  for i in range(0, steps):
+    setStep(1, 0, 1, 0)
+    time.sleep(delay)
+    setStep(0, 1, 1, 0)
+    time.sleep(delay)
+    setStep(0, 1, 0, 1)
+    time.sleep(delay)
+    setStep(1, 0, 0, 1)
+    time.sleep(delay)
+
+def backwards(delay, steps):
+  for i in range(0, steps):
+    setStep(1, 0, 0, 1)
+    time.sleep(delay)
+    setStep(0, 1, 0, 1)
+    time.sleep(delay)
+    setStep(0, 1, 1, 0)
+    time.sleep(delay)
+    setStep(1, 0, 1, 0)
+    time.sleep(delay)
+
+def setStep(w1, w2, w3, w4):
+  GPIO.output(coil_A_1_pin, w1)
+  GPIO.output(coil_A_2_pin, w2)
+  GPIO.output(coil_B_1_pin, w3)
+  GPIO.output(coil_B_2_pin, w4)
+
+while True:
+  forward(int(2) / 1000.0, int(128))
+  time.sleep(60)
+  backwards(int(2) / 1000.0, int(128))
+  time.sleep(60)
+  
+GPIO.cleanup()
